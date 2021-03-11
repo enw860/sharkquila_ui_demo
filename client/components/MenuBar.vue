539fff1c6eaa6aa13d1b4321405acf87357b33dc
lionel-wu@github.ibm.com <lionel.wu@ibm.com>
2021-03-11 12:19:40 -0500

    rename file

--- client/components/MenuBar.vue
+++ client/components/MenuBar.vue
@@ -79,7 +79,7 @@
 import store from "../store/store";
 import Logo from "./Logo.vue";
 
-import { matchByKeyword } from "../utils/keywords";
+import { matchByKeyword } from "../utils/searchControls";
 
 export default {
 	name: "MenuBar",
