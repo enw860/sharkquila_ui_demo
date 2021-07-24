7a56007ff9c4e3de78880b86f86c0dd2c077b08b
lionel-wu@github.ibm.com <lionel.wu@ibm.com>
2021-07-24 14:53:00 -0400

    init messgaes

---
+++ client/views/informational/Messages.vue
@@ -0,0 +1,113 @@
+<template>
+	<div class="MessagesDemo">
+		<ControlDemoTemplate ref="DT">
+			<div slot="overview">
+				<p>This control is used to display a piece of short message.</p>
+			</div>
+
+			<l-messages
+				slot="widgit"
+				ref="control"
+				:size="state.fontSize"
+				:messageStyle="state.messageStyle"
+			/>
+
+			<div slot="control">
+				<l-text value="Controls" size="xlarge" />
+
+				<l-label-wrapper value="Value:" size="small">
+					<l-input-text
+						slot="labelContent"
+						:value="state.value"
+						@blur="updateValue"
+						placeholder="Text"
+					/>
+				</l-label-wrapper>
+
+				<l-label-wrapper value="Font size:" size="small">
+					<l-input-single-select
+						slot="labelContent"
+						:value="state.fontSize"
+						:options="FONT_SIZES"
+						@change="updateFontSize"
+					/>
+				</l-label-wrapper>
+
+				<l-label-wrapper value="Message style:" size="small">
+					<l-input-single-select
+						slot="labelContent"
+						:value="state.messageStyle"
+						:options="MESSAGE_STYLES"
+						@change="updateMessageStyle"
+					/>
+				</l-label-wrapper>
+			</div>
+
+			<l-html-text-loader
+				slot="code"
+				type="<Vue template>"
+				:value="codeBody"
+			/>
+		</ControlDemoTemplate>
+	</div>
+</template>
+
+<script>
+import ControlDemoTemplate from "../main/ControlDemoTemplate.vue";
+
+export default {
+	name: "MessagesDemo",
+	displayName: "Messages",
+	controlMapping: [
+		{
+			keywords: ["message", "messages", "l-messages"],
+		},
+	],
+	components: {
+		ControlDemoTemplate,
+	},
+	data: function () {
+		return {
+			FONT_SIZES: [],
+			MESSAGE_STYLES: [],
+			state: {
+				value: "",
+				fontSize: "default",
+				messageStyle: "default",
+			},
+		};
+	},
+	computed: {
+		codeBody: function () {
+			return `\
+				<template>\
+					<l-messages\
+					/>\
+				</template>`;
+		},
+	},
+	methods: {
+		updateValue: function (event) {
+			this.state.value = event.target.value;
+		},
+		updateFontSize: function (event) {
+			this.state.fontSize = event.target.value;
+		},
+		updateMessageStyle: function (event) {
+			this.state.messageStyle = event.target.value;
+		},
+	},
+	mounted: function () {
+		this.$refs.DT.updateControl(this.$refs.control);
+
+		const { props } = this.$refs.control.$options || {};
+		if (props) {
+			const { size } = props;
+			this.FONT_SIZES = (size || {}).options || [];
+		}
+	},
+	updated: function () {
+		this.$refs.DT.setControlDOMStructure(this.$refs.control);
+	},
+};
+</script>
--- client/views/informational/index.js
+++ client/views/informational/index.js
@@ -6,8 +6,9 @@ import ClockDemo from "./Clock";
 import DisplayTextDemo from "./DisplayText";
 import HTMLTextLoaderDemo from "./HTMLTextLoader";
 import LinkDemo from "./Link";
+import MessagesDemo from "./Messages";
 import TimerDemo from "./Timer";
 
 export default [
-    AvatarDemo, BadgeDemo, BreadCrumbsDemo, ButtonDemo, ClockDemo, DisplayTextDemo, HTMLTextLoaderDemo, LinkDemo, TimerDemo
+    AvatarDemo, BadgeDemo, BreadCrumbsDemo, ButtonDemo, ClockDemo, DisplayTextDemo, HTMLTextLoaderDemo, LinkDemo, MessagesDemo, TimerDemo
 ]
